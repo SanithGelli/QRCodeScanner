1eac8f206863600b6f9d604dd68794a3f391e9ed
Sanith Raj <sanith.iosdev@gmail.com>
2019-10-23 16:46:08 -0700

    Added QRScannerView and HomeViewController class files

--- QRCodeScanner/QRCodeScanner/HomeViewController.swift
+++ QRCodeScanner/QRCodeScanner/HomeViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeViewController.swift
 //  QRCodeScanner
 //
 //  Created by Sanith on 10/23/19.
@@ -8,13 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
 
 }
 
